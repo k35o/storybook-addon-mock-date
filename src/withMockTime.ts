42bee75028b9ef28093e9f8761d247059675f22e
Koki Sakano <kosakanoki@gmail.com>
2024-05-23 19:16:53 +0900

    consider situations where mockingdate is not set

--- src/withMockTime.ts
+++ src/withMockTime.ts
@@ -5,6 +5,7 @@ import type {
 } from "@storybook/types";
 import FakeTimers from "@sinonjs/fake-timers";
 
+const now = new Date();
 let clock: FakeTimers.Clock | undefined;
 
 export const withMockTime = (
@@ -12,6 +13,13 @@ export const withMockTime = (
   context: StoryContext<Renderer>
 ) => {
   const mockingDate = context.parameters.mockingDate;
+
+  if (!mockingDate) {
+    if (clock) {
+      clock.setSystemTime(now);
+    }
+    return StoryFn(context);
+  }
 
   if (!clock) {
     clock = FakeTimers.install({
